41c64758062891b089f3299b6daebf20279246ed
Laukendra Singh <lingh@ezcorp.com>
2024-04-24 14:58:34 +0530

    Commit

--- src/main/java/com/impressico/lnd/employee/EmployeeController.java
+++ src/main/java/com/impressico/lnd/employee/EmployeeController.java
@@ -1,12 +1,14 @@
 package com.impressico.lnd.employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -15,8 +17,11 @@ import java.util.List;
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
-//    @Autowired
-//    private AlertClient alertClient;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${alert.service.url}")
+    private String alertUrl;
 
     @GetMapping("/")
     public ResponseEntity<List<Employee>> getAll() {
@@ -28,8 +33,17 @@ public class EmployeeController {
         return new ResponseEntity<>(employeeRepository.findById(id).orElse(null), HttpStatus.OK);
     }
 
-    /*@GetMapping("/employeeId/{employeeId}/alerts")
-    public ResponseEntity<List<AlertDto>> getEmployeeAlerts(@PathVariable("employeeId") Integer employeeId) {
-        return new ResponseEntity<>(alertClient.getEmployeeAlerts(employeeId), HttpStatus.OK);
-    }*/
+    @GetMapping("/employeeId/{employeeId}/alerts")
+    public ResponseEntity<String> getEmployeeAlerts(@PathVariable("employeeId") Integer employeeId) {
+        String result;
+        String url = alertUrl + "/alerts/employeeId/" + employeeId;
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            result = response.getBody();
+        } else {
+            result = "ERROR: Failed to fetch alerts from url: " + url;
+        }
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 }
